f8e55dae6d2e08d929a01745e11f22c2ff969f50
ayush <jaiswalayush0730@gmail.com>
2023-11-25 20:57:45 +0530

    first version

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+
+import Header from "../Components/Header";
+import Memes from "../Components/Memes";
+
+const Home = () =>{
+    return(
+        <div>
+            <Header/> 
+            <Memes/>
+        </div>
+    )
+
+}
+export default Home;
